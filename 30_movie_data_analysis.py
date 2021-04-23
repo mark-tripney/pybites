ec33b41a73bb13553a69a57144afc6538f417e62
Mark Tripney <marktripney@icloud.com>
2021-04-23 10:27:32 +0200

    Add calc_mean_score function.

--- 30_movie_data_analysis.py
+++ 30_movie_data_analysis.py
@@ -19,23 +19,36 @@ Movie = namedtuple("Movie", "title year score")
 
 
 def get_movies_by_director():
-    """Extracts all movies from csv and stores them in a dict,
-    where keys are directors, and values are a list of movies,
-    use the defined Movie namedtuple"""
-    pass
+    """Extracts all movies from csv and stores them in a dict, where keys are
+    directors and values are lists of movies.
+    Movies made before 1960 are excluded."""
+    directors = defaultdict(list)
+    with open(MOVIE_DATA, encoding="utf-8") as f:
+        for line in csv.DictReader(f):
+            try:
+                director = line["director_name"]
+                movie = line["movie_title"].replace("\xa0", "")
+                year = int(line["title_year"])
+                score = float(line["imdb_score"])
+            except ValueError:
+                continue
+            if year >= 1960:
+                movie_details = Movie(title=movie, year=year, score=score)
+            directors[director].append(movie_details)
+    return directors
+
 
 def calc_mean_score(movies):
-    """Helper method to calculate mean of list of Movie namedtuples,
-    round the mean to 1 decimal place"""
-    pass
+    """Helper method to calculate mean of list of Movie namedtuples, round the
+    mean to 1 decimal place"""
+    filmography = get_movies_by_director()[movies]
+    mean_score = sum([movie.score for movie in filmography]) / len(filmography)
+    return round(mean_score, 1)
 
 
 def get_average_scores(directors):
     """Iterate through the directors dict (returned by get_movies_by_director),
     return a list of tuples (director, average_score) ordered by highest
-    score in descending order. Only take directors into account
-    with >= MIN_MOVIES"""
+    score in descending order.
+    Only take directors into account with >= MIN_MOVIES"""
     pass
-
-
-get_movies_by_director()
